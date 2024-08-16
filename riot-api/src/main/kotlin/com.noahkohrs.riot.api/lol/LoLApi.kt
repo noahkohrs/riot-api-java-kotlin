185d73bf754476ae49f0a21be6815b6ec41cfebe
noahkohrs <noah.kohrs@bitmovin.com>
2024-08-16 20:03:52 +0200

    feat: adding league api endpoints.

--- riot-api/src/main/kotlin/com.noahkohrs.riot.api/lol/LoLApi.kt
+++ riot-api/src/main/kotlin/com.noahkohrs.riot.api/lol/LoLApi.kt
@@ -2,14 +2,15 @@ package com.noahkohrs.riot.api.lol
 
 import com.noahkohrs.riot.api.lol.champion.ChampionApi
 import com.noahkohrs.riot.api.lol.championmastery.ChampionMasteryApi
+import com.noahkohrs.riot.api.lol.league.LeagueApi
 import com.noahkohrs.riot.api.lol.leagueexp.LeagueExpApi
 import com.noahkohrs.riot.api.lol.status.StatusApi
 import com.noahkohrs.riot.api.lol.summoner.SummonerApi
 import com.noahkohrs.riot.api.values.Platform
 
 public class LoLApi(
-    apiKey: String,
-    platform: Platform,
+    private val apiKey: String,
+    private val platform: Platform,
 ) {
     @JvmField
     public val champion: ChampionApi = ChampionApi(apiKey, platform)
@@ -25,4 +26,7 @@ public class LoLApi(
 
     @JvmField
     public val leagueExp: LeagueExpApi = LeagueExpApi(apiKey, platform)
+
+    @JvmField
+    public val league: LeagueApi = LeagueApi(apiKey, platform)
 }

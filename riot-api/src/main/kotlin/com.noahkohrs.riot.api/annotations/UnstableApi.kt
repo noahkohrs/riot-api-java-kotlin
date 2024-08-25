6e2f9c5c18d2e8d066df57d7b6217ddb7c7e3856
noahkohrs <noah@kohrs.fr>
2024-08-25 16:23:40 +0200

    docs: updating todo list and documentation for the unstable api annotation.

--- riot-api/src/main/kotlin/com.noahkohrs.riot.api/annotations/UnstableApi.kt
+++ riot-api/src/main/kotlin/com.noahkohrs.riot.api/annotations/UnstableApi.kt
@@ -1,7 +1,7 @@
 package com.noahkohrs.riot.api.annotations
 
 /**
- * Marks a part of the API as unstable. This means that the API might change in the future.
+ * Marks a part of the API as unstable. This means that the API might change in soon and is still open to discussion. Feel free to use it, but be aware that it might change in the future.
  */
 @Retention(AnnotationRetention.BINARY)
 public annotation class UnstableApi()

b93c51c3342ca927b4bb0ac979d9f9434a380df6
noahkohrs <noah@kohrs.fr>
2024-09-02 16:40:22 +0200

    test: adding parallelism in tests of match api.

--- riot-api/src/test/kotlin/com/noahkohrs/riot/api/lol/match/MatchApiTest.kt
+++ riot-api/src/test/kotlin/com/noahkohrs/riot/api/lol/match/MatchApiTest.kt
@@ -3,8 +3,10 @@ package com.noahkohrs.riot.api.lol.match
 import TestInternal
 import com.noahkohrs.riot.api.RiotApi
 import com.noahkohrs.riot.api.values.Platform
-import org.junit.jupiter.api.Assertions.*
+import getTestExecutor
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import waitForCompletion
 
 class MatchApiTest {
     val riotApi = RiotApi(TestInternal.apiKey, Platform.EUW1)
@@ -14,29 +16,37 @@ class MatchApiTest {
      */
     @Test
     fun getMatchByIdStressTest() {
-        val testMatchs = mutableSetOf<String>()
-        for (p in TestInternal.somePlayers) {
-            val puuid = riotApi.account.getAccountByRiotId(p.name, p.tag).puuid
-            val matchIds = riotApi.lol.match.getMatchIdsByPuuid(puuid, count = 80)
-            testMatchs.addAll(matchIds)
+        val testMatchs = getTestMatchsPool()
+        val threadPool = getTestExecutor()
+        testMatchs.forEach { matchId ->
+            threadPool.submit {
+                val match = riotApi.lol.match.getMatchById(matchId)
+                assertNotNull(match)
+            }
         }
-        for (matchId in testMatchs) {
-            val match = riotApi.lol.match.getMatchById(matchId)
-            assertNotNull(match)
-        }
+        threadPool.waitForCompletion()
     }
 
     @Test
     fun getMatchTimelineByIdStressTest() {
+        val threadPool = getTestExecutor()
+        val testMatchs = getTestMatchsPool()
+        testMatchs.forEach { matchId ->
+            threadPool.submit {
+                val match = riotApi.lol.match.getMatchTimelineById(matchId)
+                assertNotNull(match)
+            }
+        }
+        threadPool.waitForCompletion()
+    }
+
+    private fun getTestMatchsPool(): Set<String> {
         val testMatchs = mutableSetOf<String>()
         for (p in TestInternal.somePlayers) {
             val puuid = riotApi.account.getAccountByRiotId(p.name, p.tag).puuid
             val matchIds = riotApi.lol.match.getMatchIdsByPuuid(puuid, count = 80)
             testMatchs.addAll(matchIds)
         }
-        for (matchId in testMatchs) {
-            val match = riotApi.lol.match.getMatchTimelineById(matchId)
-            assertNotNull(match)
-        }
+        return testMatchs
     }
 }

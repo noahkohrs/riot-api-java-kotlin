ed28f6ec0c97c202a9431006d4a82e6382305382
noahkohrs <noah@kohrs.fr>
2024-09-02 19:25:43 +0200

    refactor: linting + example fix

--- riot-api/src/main/kotlin/com.noahkohrs.riot.api/lol/summoner/Responses.kt
+++ riot-api/src/main/kotlin/com.noahkohrs.riot.api/lol/summoner/Responses.kt
@@ -5,12 +5,30 @@ import com.noahkohrs.riot.api.dtos.SummonerDto
 import java.util.Date
 
 public class SummonerResponse private constructor(
+    /**
+     * Encrypted account ID.
+     */
     public val id: String,
+    /**
+     * Encrypted summoner ID.
+     */
     public val accountId: String,
+    /**
+     * Encrypted PUUID.
+     */
     public val puuid: String,
+    /**
+     * The id of the summoner icon associated with the summoner.
+     */
     @LinkToStaticApi
     public val profileIconId: Int,
+    /**
+     * Date summoner was last modified specified as epoch milliseconds.
+     */
     public val revisionDate: Date,
+    /**
+     * Summoner level associated with the summoner.
+     */
     public val summonerLevel: Long,
 ) {
     internal companion object {
